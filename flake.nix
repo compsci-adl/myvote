4995632cfbc887e823676eb92f44d3ad7c3aee83
Patrick Thompson <ori@kek.church>
2024-11-23 17:49:18 +1030

    chore: Update flake and package frontend

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "CS Club Voting API";
+  description = "CS Club Voting App and API";
 
   inputs = {
     nixpkgs.url = "nixpkgs";
@@ -16,7 +16,7 @@
       system: let
         pkgs = import nixpkgs {inherit system;};
         # Python package dependencies
-        pyDeps = with pkgs.python312Packages; [
+        pyPackages = with pkgs.python312Packages; [
             sqlmodel
             alembic
             aiosqlite
@@ -27,21 +27,41 @@
             uvicorn
             fastapi
           ];
-        # All dependencies, including non-python-packages
-        deps = with pkgs;
-          [
+          pyDeps = with pkgs; [
             python312
-            uv
-          ] ++ pyDeps;
+          ] ++ pyPackages;
       in rec {
 
-        # Construct default dev package, which runs setup.py to find packages
-        packages.default = pkgs.python312Packages.buildPythonApplication rec {
+        # This is mainly for use in CI or deployment, just use pnpm for development
+        # Normally we are using pnpm, but for CI and deployment we want the nix build,
+        # so we need an npm lockfile. Solution is rn -rf node_modules and run npm i --package-lock-only
+        packages.frontend = pkgs.buildNpmPackage {
+          pname = "myvote";
+          version = "0.0.0";
+          npmDepsHash = "sha256-8pKzROHa1eQ2/RVxSz3SA7POpXvRPGPtRz/KnWcWdlw=";
+          src = ./frontend;
+          postInstall = ''
+            mkdir -p $out/bin
+            cat <<EOF > $out/bin/myvote
+            #!/usr/bin/env sh
+            cd $out/lib/node_modules/myvote
+            ${pkgs.http-server}/bin/http-server dist
+            EOF
+            chmod +x $out/bin/myvote
+          '';
+          # Maybe if we use this in prod replace this with some other http server?
+          propagatedBuildInputs = [pkgs.http-server];
+
+        };
+
+        packages.default = packages.backend;
+
+        packages.backend-dev = pkgs.python312Packages.buildPythonApplication rec {
           pname = "vapi";
           version = "0.0.0";
 
-          propagatedBuildInputs = pyDeps;
-          src = ./.;
+          propagatedBuildInputs = pyPackages;
+          src = ./backend;
 
           # Use a bash variable for the run mode so we can override it
           preInstall = "mode=dev";
@@ -58,12 +78,12 @@
         };
 
         # Our prod package is the same but with mode=run instead of mode=dev
-        packages.prod = packages.default.override {preInstall = "mode=run";};
+        packages.backend-prod = packages.default.override {preInstall = "mode=run";};
 
         # We dont require a C compiler here so we use mkShellNoCC
         devShells.default = pkgs.mkShellNoCC {
           # Simply forward all required packages so we can use make and fastapi commands easily
-          packages = deps;
+          packages = pyDeps;
           name = "vapi";
 
           # Shellhook to sync uv is below if using non-nixpkgs python packages
