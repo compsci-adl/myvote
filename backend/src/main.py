c46469a097c4694c58e7eabe75e5d09f96b634fa
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2025-02-16 18:58:05 +1030

    chore(backend): Remove temporary election creation code from lifespan function

--- backend/src/main.py
+++ backend/src/main.py
@@ -21,24 +21,6 @@ async def lifespan(app: FastAPI):
         async with engine.begin() as conn:
             await conn.run_sync(SQLModel.metadata.create_all)
 
-    # TEMP: Create the election
-    # async with AsyncSession(engine) as session:
-    #     election = await session.execute(select(Election))
-    #     election = election.scalars().first()
-    #     if election is None:
-    #         n = datetime.now(timezone.utc)
-    #         election = Election(
-    #             id=0,
-    #             name="Test Election",
-    #             nomination_end=n,
-    #             nomination_start=n,
-    #             voting_end=n,
-    #             voting_start=n,
-    #             status=ElectionStatus.PreRelease,
-    #         )
-    #         session.add(election)
-    #         await session.commit()
-
     yield
 
     # Code here is executed when the app is dying, use to clean things up
