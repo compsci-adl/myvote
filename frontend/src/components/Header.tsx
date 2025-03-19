626eee9d98e3f84f9b7b5f0186d5f6909f6f4f1a
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2025-03-19 11:01:33 +1030

    feat: Make header optimised for mobile

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -40,64 +40,68 @@ export const Header = () => {
 	};
 
 	return (
-		<Navbar
-			isBordered
-			maxWidth="xl"
-			position="static"
-			classNames={{ wrapper: 'px-4' }}
-		>
-			<NavbarBrand>
-				<img src="/favicon.svg" alt="Logo" className="mr-2 w-6" />
-				<h1 className="font-bold text-inherit">MyVote</h1>
-			</NavbarBrand>
+		<div className="flex h-full flex-col">
+			<Navbar maxWidth="xl" position="static" classNames={{ wrapper: 'px-4' }}>
+				<NavbarBrand>
+					<img src="/favicon.svg" alt="Logo" className="mr-2 w-6" />
+					<h1 className="font-bold text-inherit">MyVote</h1>
+				</NavbarBrand>
 
-			<NavbarContent justify="center">
-				{isUserLoggedIn && (
-					<ButtonGroup>
-						<Button
-							className={clsx({ 'bg-primary': isActive('/voting') })}
-							onPress={() => router.navigate({ to: '/voting' })}
-						>
-							Voting
-						</Button>
-						<Button
-							className={clsx({ 'bg-primary': isActive('/candidates') })}
-							onPress={() => router.navigate({ to: '/candidates' })}
-						>
-							Candidates
-						</Button>
-						{isAdmin() && (
-							<Button
-								className={clsx({ 'bg-primary': isActive('/admin') })}
-								onPress={() => router.navigate({ to: '/admin' })}
-							>
-								Admin
+				<NavbarContent justify="end" className="flex items-center gap-4">
+					{initializationError && (
+						<span className="text-red-500">
+							{initializationError.isAuthServerLikelyDown
+								? 'Auth server is down'
+								: `Auth Error: ${initializationError.message}`}
+						</span>
+					)}
+
+					{isUserLoggedIn ? <LoggedInAuthButton /> : <NotLoggedInAuthButton />}
+
+					<NavbarItem>
+						<Tooltip content="Toggle Dark Mode" size="sm">
+							<Button {...HEADER_BUTTON_PROPS} onPress={toggleIsDarkMode}>
+								{isDarkMode ? '🌚' : '🌞'}
 							</Button>
-						)}
-					</ButtonGroup>
-				)}
-			</NavbarContent>
+						</Tooltip>
+					</NavbarItem>
+				</NavbarContent>
+			</Navbar>
 
-			<NavbarContent justify="end">
-				{initializationError && (
-					<span className="text-red-500">
-						{initializationError.isAuthServerLikelyDown
-							? 'Auth server is down'
-							: `Auth Error: ${initializationError.message}`}
-					</span>
-				)}
-
-				{isUserLoggedIn ? <LoggedInAuthButton /> : <NotLoggedInAuthButton />}
-
-				<NavbarItem>
-					<Tooltip content="Toggle Dark Mode" size="sm">
-						<Button {...HEADER_BUTTON_PROPS} onPress={toggleIsDarkMode}>
-							{isDarkMode ? '🌚' : '🌞'}
-						</Button>
-					</Tooltip>
-				</NavbarItem>
-			</NavbarContent>
-		</Navbar>
+			<Navbar
+				isBordered
+				maxWidth="xl"
+				position="static"
+				classNames={{ wrapper: 'px-4' }}
+			>
+				<NavbarContent justify="center" className="w-full">
+					{isUserLoggedIn && (
+						<ButtonGroup className="mx-auto">
+							<Button
+								className={clsx({ 'bg-primary': isActive('/voting') })}
+								onPress={() => router.navigate({ to: '/voting' })}
+							>
+								Voting
+							</Button>
+							<Button
+								className={clsx({ 'bg-primary': isActive('/candidates') })}
+								onPress={() => router.navigate({ to: '/candidates' })}
+							>
+								Candidates
+							</Button>
+							{isAdmin() && (
+								<Button
+									className={clsx({ 'bg-primary': isActive('/admin') })}
+									onPress={() => router.navigate({ to: '/admin' })}
+								>
+									Admin
+								</Button>
+							)}
+						</ButtonGroup>
+					)}
+				</NavbarContent>
+			</Navbar>
+		</div>
 	);
 };
 
