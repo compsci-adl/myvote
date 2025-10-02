642337cc68bc4de5759ef1e7040e81ced8a2fcff
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2025-10-02 16:19:06 +0930

    feat(ci): Add CI workflows for development and production, including linting, testing, and Docker build processes

--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,8 @@
 import NextAuth from 'next-auth';
-import { JWT } from 'next-auth/jwt';
 import Keycloak from 'next-auth/providers/keycloak';
 
 declare module 'next-auth' {
     interface Session {
-        accessToken?: string;
-    }
-}
-
-declare module 'next-auth/jwt' {
-    interface JWT {
         accessToken?: string;
     }
 }
