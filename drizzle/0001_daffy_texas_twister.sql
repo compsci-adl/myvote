8be474b60e9ff3e1ece112dcd530df0047138afd
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2025-10-02 14:55:49 +0930

    chore: Remove obsolete SQL migration files and related metadata

--- drizzle/0001_daffy_texas_twister.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `election` DROP COLUMN `nomination_start`;--> statement-breakpoint
-ALTER TABLE `election` DROP COLUMN `nomination_end`;--> statement-breakpoint
-ALTER TABLE `election` DROP COLUMN `voting_start`;--> statement-breakpoint
-ALTER TABLE `election` DROP COLUMN `voting_end`;
